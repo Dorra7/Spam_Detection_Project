b767fb9f448a5899ce42f060cbec307299b92b26
Dorra Bahri <dorra.bahri370@gmail.com>
2025-12-03 19:40:52 +0100

    requirments satisfied

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projet d'Apprentissage Artificiel de Détection de Spams',
+    author='Dorra BAHRI',
+    license='MIT',
+)
